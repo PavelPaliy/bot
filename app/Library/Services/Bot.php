3eb354c970e4033bdd35b85e601974e9091df533
Павел Палий <paliipavel@gmail.com>
2020-01-17 15:41:44 +0200

    путин

--- app/Library/Services/Bot.php
+++ app/Library/Services/Bot.php
@@ -9,9 +9,9 @@ class Bot
     public function send_message($chat_id, $text, $url)
     {
       $curl = new Curl();
-      $curl->post($url, array(
+      /*$curl->post($url, array(
           'chat_id':$chat_id, 'text':$text
-      ));
+      ));*/
     }
     public function huy($a){
     	return $a;
