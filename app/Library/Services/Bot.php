5ad608603b4482b19030d850e3b697bcc8302434
Павел Палий <paliipavel@gmail.com>
2020-01-17 15:32:09 +0200

    путин

--- app/Library/Services/Bot.php
+++ app/Library/Services/Bot.php
@@ -6,7 +6,7 @@ use \Curl\Curl;
 
 class Bot
 {
-    public function send_message(chat_id, text, url)
+    public function send_message($chat_id, $text, $url)
     {
       $curl = new Curl();
       $curl->post($url, array(
