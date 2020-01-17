e24426e1837fab28829c749fcb36c534030d8fb6
Павел Палий <paliipavel@gmail.com>
2020-01-17 15:37:40 +0200

    путин

--- app/Library/Services/Bot.php
+++ app/Library/Services/Bot.php
@@ -8,8 +8,8 @@ class Bot
 {
     public function send_message($chat_id, $text, $url)
     {
-      /*$curl = new Curl();
-      $curl->post($url, array(
+      $curl = new Curl();
+      /*$curl->post($url, array(
           'chat_id':$chat_id, 'text':$text
       ));*/
     }
