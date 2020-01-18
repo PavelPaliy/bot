f7d1e973e089bddf05b279fc50982f0bba7c81ac
Павел Палий <paliipavel@gmail.com>
2020-01-18 22:22:48 +0200

    trash

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -73,13 +73,13 @@ class BotController extends Controller
                                            $link = 'https://2ch.hk/'.$board.'/res/'.explode('/', $threads[$i]['files'][0]['path'])[3].'.html'.PHP_EOL;
                                            $text .= $link;
                                            if(count(Link::where('name', $link)->get())==0){
-                                                $link_obj = new Link();
+                                                /*$link_obj = new Link();
                                                 $link_obj->name = $link;
                                                 $tag_obj = Tag::where('name', $tag)->first()->chat()->where('chat_id', $chat_id)->firstOrFail();
                                                 $link_obj->tags()->sync([
                                                     $tag_obj
                                                 ]);
-                                                $link_obj->save();
+                                                $link_obj->save();*/
 
                                            }
                                            else{
