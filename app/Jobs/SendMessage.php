41215d54be4ac3d00f871e6c2f2cb673ac5c519e
Павел Палий <paliipavel@gmail.com>
2020-01-20 14:59:52 +0200

    huy shedule

--- app/Jobs/SendMessage.php
+++ app/Jobs/SendMessage.php
@@ -4,10 +4,14 @@ namespace App\Jobs;
 use App\Library\Services\Bot;
 use App\Chat;
 
-public function __construct(){
-	$bot = new Bot();
-	$url = "https://api.telegram.org/bot831592934:AAEz6KykUmMg0i8q0HNJ_DwhILCDcnJfMCQ/";
-	$chat_id = Chat::where('id', 1)->firstOrFail()->chat_id;
-	$text = 'huy';
-    $bot->send_message($chat_id, $text, $url."sendMessage");
+
+class SendMessage{
+	public function __construct(){
+		$bot = new Bot();
+		$url = "https://api.telegram.org/bot831592934:AAEz6KykUmMg0i8q0HNJ_DwhILCDcnJfMCQ/";
+		$chat_id = Chat::where('id', 1)->firstOrFail()->chat_id;
+		$text = 'huy';
+	    $bot->send_message($chat_id, $text, $url."sendMessage");
+	}
 }
+
